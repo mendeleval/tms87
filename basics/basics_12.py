5a993e54c2ba5d6da4eb28aa47f642c501241e0b
Aleksandr <mendelev.basic@gmail.com>
2023-03-05 14:20:04 +0300

    DZ

---
+++ basics/basics_12.py
@@ -0,0 +1,11 @@
+# Запросить у пользователя два целых числа.
+# Вывести строку вида “2 плюс 3 равно 5”
+# Примечание: после ввода переменных преобразовать переменные к типу int
+#  >> first_number = int(first_number)
+
+print('Введите два числа: ')
+
+first_number = int(input())
+second_number = int(input())
+
+print(first_number + second_number)
