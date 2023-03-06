b07c176e37e5a6ac53672c03a574804eb961d807
Aleksandr <mendelev.basic@gmail.com>
2023-03-06 22:36:23 +0300

    DZ

--- conditions/conditions_06.py
+++ conditions/conditions_06.py
@@ -1,9 +1,11 @@
 # Ввести почтовый адрес. Проверить доменное имя.
 # В случае если оно gmail.com, вывести на экран имя почтового ящика.
 # Иначе вывести надпись “DOMAIN NAME is not supported’
-
+#
 text = input('Введите почтовый адрес: ')
-if "gmail.com" in text:
+word = text.split('@')
+
+if word[-1] == 'gmail.com':
     print(text)
 
 else:
--- loops/loops_01.py
+++ loops/loops_01.py
@@ -1,1 +1,4 @@
 # Выведите на экран n раз фразу "I am a programmer". Число n вводит пользователь.
+
+text = int(input('Введите кол-во повторений: '))
+print('I am a programmer ' * text)
