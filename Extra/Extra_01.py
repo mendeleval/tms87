f26575709b55c828a95b3efdf1559f7720f20b92
Aleksandr <mendelev.basic@gmail.com>
2023-03-09 20:03:01 +0300

    DZ

--- Extra/Extra_01.py
+++ Extra/Extra_01.py
@@ -6,11 +6,28 @@
 # 7*. Найти верхнюю и нижнюю квартиль
 
 arr = [1, 2, 3, 4, 4, 4, 5, 5, 2]
-print(sum(arr))
+# count = 0
+# result = 0
+# while count < len(arr):
+#     result += arr[count]
+#     count += 1
 #
+# print(result)
 
-#
-# arr = (1**2, 2**2, 3**2, 4**2, 4**2, 4**2, 5**2, 5**2, 2**2)
-# print(arr)
-#
-#
+# result = 0
+# while arr:
+#     result += arr.pop(0)
+# print(result)
+
+
+# Найти верхнюю и нижнюю квартиль = 0.75 * (n +1)     n - кол-во элементов
+arr = [1, 2, 3, 4, 4, 4, 5, 5, 2]
+arr.sort()
+print(arr)
+index_v_dkvar = 0.75 * (len(arr) - 1)
+int_index = int(index_v_dkvar)
+if index_v_dkvar % 1:
+    print(arr[int_index])
+else:
+    result = (arr[int_index] + arr[int_index + 1]) / 2
+    print(result)
