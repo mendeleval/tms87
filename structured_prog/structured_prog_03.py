7cae76c16fe600af26fdebf714ebb3851471588b
Aleksandr <mendelev.basic@gmail.com>
2023-03-09 21:25:53 +0300

    DZ

---
+++ structured_prog/structured_prog_03.py
@@ -0,0 +1,17 @@
+# Получить на ввод количество рублей и копеек и вывести в правильной форме,
+# например, 3 рубля, 1 рубль 25 копеек, 3 копейки
+
+my_rub = int(input('Введите кол-во рублей: '))
+my_cen = int(input('Введите кол-во копеек: '))
+
+# if my_rub > 0:
+#     print(f'Рублей - {my_rub}')
+# if my_cen > 0:
+#     print(f'Копеек - {my_cen}')
+
+if (my_rub != 0) and ( my_cen != 0 ):
+    print(f'{my_rub} rub. {my_cen} cen')
+elif (my_rub != 0) and (my_cen == 0):
+    print(f' {my_rub} rub.')
+else:
+    print(f'{my_cen} cen.')
