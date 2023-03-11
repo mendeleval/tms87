9a269d97ba34ff07512940d24299b8862d4271f6
Aleksandr <mendelev.basic@gmail.com>
2023-03-11 12:52:36 +0300

    DZ

---
+++ loops/loops_09.py
@@ -0,0 +1,12 @@
+# Создать квадратную матрицу размерностью n и заполнить ее случайными значениями от 1 до 9.
+from random import randint
+
+n = int(input('Размер матрицы: '))
+
+matrix = []
+for i in range(n):
+    matrix.append([randint(1, 9)] * n)
+
+print(matrix)
+
+
