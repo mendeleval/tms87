9947337dcfec45bf222ff7c273aa756b77b66b27
Aleksandr <mendelev.basic@gmail.com>
2023-03-13 19:55:07 +0300

    DZ

--- extra/extra_01.py
+++ extra/extra_01.py
@@ -1,7 +1,19 @@
-arr = [1, 2, 3, 4, 4, 4, 5, 5, 2]
-arr_len = len(arr)
-for run in range(arr_len - 1):
-    for i in range(arr_len - 1):
-        if arr[i] > arr[i + 1]:
-            arr[i], arr[i + 1] = arr[i + 1], arr[i]
-print(arr)
+# arr = [1, 2, 3, 4, 4, 4, 5, 5, 2]
+# arr_len = len(arr)
+# for run in range(arr_len - 1):
+#     for i in range(arr_len - 1):
+#         if arr[i] > arr[i + 1]:
+#             arr[i], arr[i + 1] = arr[i + 1], arr[i]
+# print(arr)
+
+
+nums = [3, 1, 3, 7, 4, 4, 5, 5, 2]
+swapped = True
+while swapped:
+    swapped = False
+    for i in range(len(nums) - 1):
+        if nums[i] > nums[i + 1]:
+            nums[i], nums[i + 1] = nums[i + 1], nums[i]
+            swapped = True
+
+print(nums)
