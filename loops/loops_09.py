b2d7a3ab6b5d4f2dee524d6c8acba2e81210e210
Aleksandr <mendelev.basic@gmail.com>
2023-03-13 18:59:15 +0300

    DZ

--- loops/loops_09.py
+++ loops/loops_09.py
@@ -1,17 +1,19 @@
 # Создать квадратную матрицу размерностью n и заполнить ее случайными значениями от 1 до 9.
+
 from random import randint
 
 n = int(input('Размер матрицы: '))
 
 matrix = []
-# for i in range(n):
-#     matrix.append([randint(1, 9)] * n)
-# print(matrix)
-
+for i in range(n):
+    column = []
+    for j in range(n):
+        column.append(randint(1, 9))
+    matrix.append(column)
 
 for i in range(len(matrix)):
-    for j in range(len([i])):
-        ''' вместо новой строки печатаем пробел '''
-        print(M[i][j], end=' ')
-    ''' переход на новую строку после печати строки матрицы '''
+    for j in range(len(matrix[i])):
+        print(matrix[i][j], end=' ')
     print()
+
+# сделал но не понял
