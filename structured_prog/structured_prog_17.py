008a08ade409c1e87ce74bbc5adb71ce78966fd0
Aleksandr <mendelev.basic@gmail.com>
2023-03-17 19:16:37 +0300

    DZ

--- structured_prog/structured_prog_17.py
+++ structured_prog/structured_prog_17.py
@@ -1,15 +1,23 @@
 # В массиве целых чисел с количеством элементов 19 определить максимальное число
 # и заменить им все четные по значению элементы.
 
-my_list = [1, 2, 3, 4, 70, 6, 15, 20, 66, 76, 87, 75, 80, 77, 12, 11, 100, 64, 13]
-max_number = []
-
+nums = [1, 2, 3, 4, 70, 6, 15, 20, 66, 76, 87, 75, 80, 77, 12, 11, 100, 64, 13]
+print(nums)
 swapped = True
 while swapped:
     swapped = False
-    for i in my_list:
-        if my_list[i] > my_list[i + 1]:
-            my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
+    for i in range(len(nums) - 1):
+        if nums[i] > nums[i + 1]:
+            nums[i], nums[i + 1] = nums[i + 1], nums[i]
             swapped = True
 
-print(my_list)
+max_number = nums[-1]
+
+print(nums)
+print(f'max num - {max_number}')
+
+for i in range(len(nums) - 1):
+    if nums[i] % 2 == 0:
+        nums[i] = max_number
+
+print(nums)
---
+++ structured_prog/structured_prog_19.py
@@ -0,0 +1,8 @@
+# Дана целочисленная квадратная матрица. Найти в каждой строке наибольший элемент и поменять его местами
+# с элементом главной диагонали.
+
+str_matrix = int(input('enter count str - '))
+col_matrix = int(input('enter count col - '))
+matrix = []
+for i in matriix:
+    collumn
