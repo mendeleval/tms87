ec18089ef1051344568523eaafedc01271177381
Aleksandr <mendelev.basic@gmail.com>
2023-03-17 15:42:43 +0300

    DZ

--- structured_prog/structured_prog_17.py
+++ structured_prog/structured_prog_17.py
@@ -4,7 +4,12 @@
 my_list = [1, 2, 3, 4, 70, 6, 15, 20, 66, 76, 87, 75, 80, 77, 12, 11, 100, 64, 13]
 max_number = []
 
-for i in my_list:
-    if i > i[1]:
-        i = len(my_list) - 1
+swapped = True
+while swapped:
+    swapped = False
+    for i in my_list:
+        if my_list[i] > my_list[i + 1]:
+            my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
+            swapped = True
+
 print(my_list)
