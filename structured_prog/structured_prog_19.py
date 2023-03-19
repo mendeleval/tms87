87cd419ede6d1a4ca9620d70a15e8dfccc4c28a6
Aleksandr <mendelev.basic@gmail.com>
2023-03-19 20:57:23 +0300

    DZ

--- structured_prog/structured_prog_19.py
+++ structured_prog/structured_prog_19.py
@@ -1,8 +1,16 @@
-# Дана целочисленная квадратная матрица. Найти в каждой строке наибольший элемент и поменять его местами
-# с элементом главной диагонали.
-
-str_matrix = int(input('enter count str - '))
-col_matrix = int(input('enter count col - '))
-matrix = []
-for i in matriix:
-    collumn
+# # Дана целочисленная квадратная матрица. Найти в каждой строке наибольший элемент и поменять его местами
+# # с элементом главной диагонали.
+# from random import randint
+#
+# size_matrix = int(input('enter count str - '))
+# matrix = []
+# for i in range(size_matrix):
+#     column = []
+#     for j in range(size_matrix):
+#         column.append(randint(1, 9))
+#     matrix.append(column)
+#     print(column)
+#
+name = int(input("Введите имя - "))
+end =name * 2
+print(f'Привет, {end}  спасибо, что оставили отзыв')
