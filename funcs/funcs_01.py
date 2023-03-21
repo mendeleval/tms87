b0cd074ab2f357ea204e797dd37cd1d7ca1f9b1d
Aleksandr <mendelev.basic@gmail.com>
2023-03-21 11:24:51 +0300

    DZ

--- funcs/funcs_01.py
+++ funcs/funcs_01.py
@@ -2,11 +2,24 @@
 # Создать список из 5 имен. Вызвать функцию для каждого элемента списка в цикле.
 
 
-def my_func():
-    arr = ["Dima", "Maksim", "Valera", "Ira", "Valik"]
-    for i in arr:
-        print(f'Hello {i}')
-
-my_func()
+# def my_func():
+#     arr = ["Dima", "Maksim", "Valera", "Ira", "Valik"]
+#     for i in arr:
+#         print(f'Hello {i}')
+#
+# my_func()
 
 # Сделать через main
+
+def my_func(name: str):
+    print(f'Hello {name}')
+
+
+def main():
+    arr = ["Dima", "Maksim", "Valera", "Ira", "Valik"]
+    for i in arr:
+        my_func(i)
+
+
+if __name__ == '__main__':
+    main()
--- funcs/funcs_02.py
+++ funcs/funcs_02.py
@@ -4,12 +4,15 @@
 # Сумма всех элементов
 # Нахождение максимального элемента
 # Нахождение минимального элемента.
+
 from random import randint
 
 
-
-
 def create_func():
-    my_matrix = [[randint(1,9) for _ in range(4)]for _ in range(4)]
-    print(my_matrix)
-create_func()
+    x = int(input('enter size - '))
+    my_matrix = [[randint(1, 9) for _ in range(x)] for _ in range(x)]
+
+
+# create_func()
+
+
