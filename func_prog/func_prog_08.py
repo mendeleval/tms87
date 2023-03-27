8768a5388de54e767ac135a62ff5682dad711f04
Aleksandr <mendelev.basic@gmail.com>
2023-03-27 22:06:00 +0300

    DZ

--- func_prog/func_prog_08.py
+++ func_prog/func_prog_08.py
@@ -1,4 +1,25 @@
 # Написать декоратор, который будет выводить время выполнения функции
 
 from datetime import datetime
-time = datetime.now()
+from functools import wraps
+from time import sleep
+
+
+def my_dec(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        start_time = datetime.now()
+        result = func()
+        finish_time = datetime.now()
+        delta = finish_time - start_time
+        print(delta)
+        return result
+    return inner
+
+
+@my_dec
+def very_important_func():
+    sleep(2)
+    print("very_important_func")
+
+very_important_func()
